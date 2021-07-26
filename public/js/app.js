8d6611d8e5f02090bf228626fde7ffafc02e9b8c
MarcelPoots <m.poots@quicknet.nl>
2021-07-26 16:14:58 +0200

    Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,9 +13,9 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading....'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
-            console.log(data)
+            //console.log(data)
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
